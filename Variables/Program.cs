be8c71a7e511847cf5b97c7bcc3427af9b921a39
Stacey Teltser <stacey.teltser@gmail.com>
2020-04-30 14:24:37 -0500

    created basic arithemtic equation

--- Variables/Program.cs
+++ Variables/Program.cs
@@ -6,7 +6,15 @@ namespace Variables
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int x;
+            int y;
+
+            x = 7;
+            y = x + 3;
+
+            Console.WriteLine(y);
+            Console.ReadLine();
+
         }
     }
 }
